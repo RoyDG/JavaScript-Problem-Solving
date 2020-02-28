ea24bfa6c1256f0ed14b5e3f327dad67fc4489f4
RoyDg <rdg01834@gmail.com>
2020-02-28 21:00:59 +0600

    updated

--- dice.js
+++ dice.js
@@ -13,11 +13,12 @@ console.log(randomNumber);
 var output = Math.round(randomNumber);
 console.log(output);
 
+//under loop
 for(i=0; i<5; i++){
     var randomNumber = Math.random()*6;
     var roundNumber = Math.round(randomNumber);
     console.log(roundNumber);
 }
-
+//print under 100 to 200
 var result = 100 + Math.random() * 100;
 console.log(result);
