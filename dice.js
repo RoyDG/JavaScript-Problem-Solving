6c0725ecc6dfc39bd1ba9b150f9aceb5eee7a62d
RoyDg <rdg01834@gmail.com>
2020-02-28 20:57:22 +0600

    adding random numbers examples

---
+++ dice.js
@@ -0,0 +1,23 @@
+var dice = Math.random()*10;
+console.log(dice);
+
+var randomNumber = Math.random()+10;
+console.log(randomNumber);
+
+var randomNumber = Math.random()-10;
+console.log(randomNumber);
+
+var randomNumber = Math.random()/10;
+console.log(randomNumber);
+
+var output = Math.round(randomNumber);
+console.log(output);
+
+for(i=0; i<5; i++){
+    var randomNumber = Math.random()*6;
+    var roundNumber = Math.round(randomNumber);
+    console.log(roundNumber);
+}
+
+var result = 100 + Math.random() * 100;
+console.log(result);
---
+++ random.js
@@ -0,0 +1,16 @@
+//Math.floor makes lower and Math.celi makes upper value
+var number = 2.752424;
+var number2 = 2.475857;
+var result = Math.floor(number);
+console.log(result);
+
+var result1 = Math.ceil(number);
+console.log(result1);
+
+//Math.round makes upper or lower value
+var result2 = Math.round(number);
+console.log(result2);
+
+var result3 = Math.round(number2);
+console.log(result3);
+
